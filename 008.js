09fb5240f5dee586a2afa73309836296d5e23b7d
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2024-01-07 20:19:25 +0530

    Update 008.js

--- 008.js
+++ 008.js
@@ -1,1 +1,10 @@
+let a=7;
+console.log("a =",a);
+a=8;
+console.log("a =",a);
+const b=7;
+console.log("b =",b);
+// b=8;
+// console.log("b =",b);
 
+// these two lines will gove because the constant vatiable cannot be reinititalized.
