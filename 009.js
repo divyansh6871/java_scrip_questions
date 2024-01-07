0271c4bdd9e44420c4445d10d3d4cd67d1eb8dd0
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2024-01-07 20:19:55 +0530

    Update 009.js

--- 009.js
+++ 009.js
@@ -1,1 +1,15 @@
-
+console.log(8);
+console.log("HELLO");
+let X="Divyansh Chauhan";
+console.log(X);
+function sayhello(){
+    return 'hello world';
+}
+console.log(sayhello);
+let name="Deepu";
+console.log("Hello"+name);
+let obj={
+    name: 'Kartik Chauhan',
+    age: 19
+}
+console.log(obj);
