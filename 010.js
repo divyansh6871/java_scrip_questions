481bff89c9a86de44e0df707a1652f8066f06e5d
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2024-01-07 20:20:22 +0530

    Update 010.js

--- 010.js
+++ 010.js
@@ -1,1 +1,6 @@
-
+let a=Number.parseInt(prompt("Enter the coefficient of x to the power 2 : "))
+let b=Number.parseInt(prompt("Enter the coefficient of x to the power 1 : "))
+let c=Number.parseInt(prompt("Enter the coefficient of x to the power 0 : "))
+let d=b*b-4*a*c;
+console.log("root 1=",(-b+Math.sqrt(d))/2*a);
+console.log("root 2=",(-b-Math.sqrt(d))/2*a);
