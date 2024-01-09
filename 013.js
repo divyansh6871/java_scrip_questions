bda859ba42389a6033e917360a176aa917299b2b
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2024-01-09 07:03:04 +0530

    Add files via upload

---
+++ 013.js
@@ -0,0 +1,11 @@
+let a=Number.parseInt(prompt("Enter the number 1 : "));
+let b=Number.parseInt(prompt("Enter the number 2 : "));
+let c=Number.parseInt(prompt("Enter the number 3 : "));
+if(a>b&&a>c)
+console.log("a is greatest");
+else{
+    if(b>c)
+console.log("b is greatest");
+else
+console.log("c is greatest");
+}
