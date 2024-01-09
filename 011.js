339f4a09177c54c942dc5723e1645d9f5e662c18
divyansh6871 <140478510+divyansh6871@users.noreply.github.com>
2024-01-09 07:02:41 +0530

    Add files via upload

---
+++ 011.js
@@ -0,0 +1,7 @@
+let a=Number.parseInt(prompt("ENter the number : "));
+if(a<0)
+console.log("Number is less than 0");
+else if(a==0)
+console.log("Number is 0");
+else
+console.log("NUmber is positive number");
